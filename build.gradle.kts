b5f4073ddbd025061bd59b06a261d4afa53e9d04
Arfin Hosain <developerarfin@gmail.com>
2024-01-07 10:27:11 +0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val kotlin_version by extra("1.9.20")
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+    repositories {
+        mavenCentral()
+    }
+}
+
+
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+
+}
